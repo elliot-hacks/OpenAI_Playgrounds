daebe0e62604706e52af7176cd97464717e983fe
elliot-hacks <thisguyhack@gmail.com>
2024-04-14 07:51:09 -1000

    Using promptlayer to track AI billing and requests

---
+++ 05_promptLayerAPI.py
@@ -0,0 +1,24 @@
+import os
+import promptlayer
+
+
+# Prompt layer tracks openai requests can be useful for billing of tokens 
+promptlayer.api_key = os.getenv("PROMPTLAYER_API_KEY")
+OpenAI = promptlayer.openai.OpenAI
+client = OpenAI()
+OpenAI.api_key = os.getenv("OPENAI_API_KEY")
+
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are an AI."},
+    {"role": "user", "content": "Compose a poem please."}
+  ],
+  pl_tags=["getting-started"]
+)
+
+
+msg=completion.choices[0].message.content
+print(msg)
+
